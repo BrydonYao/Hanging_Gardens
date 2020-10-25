70d34454924d27f1b8b3b9c1cb07cb4f1318f3aa
Brydon Yao <zhy204@nyu.edu>
2020-10-25 18:43:03 -0400

    Final

---
+++ Assets/scr_R.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class scr_R : MonoBehaviour
+{
+    public string level_name;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            SceneManager.LoadScene(level_name);
+        }
+    }
+}
